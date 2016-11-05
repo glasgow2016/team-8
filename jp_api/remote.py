1c9a5c98cb342e8bda036d2bdc0d35632949db39
xz32 <xz32@users.noreply.github.com>
2016-11-05 23:08:41 +0000

    Jsonify

--- jp_api/remote.py
+++ jp_api/remote.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 from sensors.humiture import Humiture as humiture
@@ -7,34 +7,30 @@ from sensors.rain_detection import RainDetection as rain_detection
 from sensors.photoresistor import PhotoResistor as photoresistor
 from sensors.rgb_led import RgbLed as rgb_led
 
-@app.route('/')
-def index():
-    return "Hey! Human"
-
 @app.route('/sensor/humidity')
 def humidity():
     humiditySensor = humiture()
-    return humiditySensor.getHumidityInPercent()
+    return jsonifyhumiditySensor.getHumidityInPercent())
 
 @app.route('/sensor/temperature')
 def temperature():
     humiditySensor = humiture()
-    return humiditySensor.getTemperatureInCeluis()
+    return jsonifyhumiditySensor.getTemperatureInCeluis())
 
 @app.route('/sensor/rain')
 def rain():
     rainSensor = rain_detection()
-    return rainSensor.isRaining()
+    return jsonify(rainSensor.isRaining())
 
 @app.route('/sensor/light')
 def light():
     lightSensor = photoresistor()
-    return lightSensor.getLightValue()
+    return jsonify(lightSensor.getLightValue())
 
 @app.route('/sensor/led/<r>/<g>/<b>')
 def led(r, g, b):
     rgbLed = rgb_led()
-    return rgbLed.setColor(r, g, b)
+    return jsonify(rgbLed.setColor(r, g, b))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
