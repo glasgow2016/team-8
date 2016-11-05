04178821e02445f608d7b8d0687ffbedd15a0fc7
xz32 <xz32@users.noreply.github.com>
2016-11-05 23:39:09 +0000

    Adding local sensor tests

---
+++ jp_api/local.py
@@ -0,0 +1,31 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+from sensors.laser import Laser as laser
+from sensors.lcd import Lcd as lcd
+from sensors.ultrasonic_ranging import UltrasonicRanging as ultrasound
+
+@app.route('/sensor/laser/<on>')
+def laser(on):
+    laserPointer = laser()
+    if on == '1':
+        laserPointer.turnOn()
+    elif on == '0':
+        laserPointer.turnOff()
+    return jsonify(True)
+
+@app.route('/sensor/ultrasound')
+def ultrasound():
+    ultrasoundSensor = ultrasound()
+    return jsonify(ultrasoundSensor.getDistanceInCm())
+
+@app.route('/sensor/lcd/<row1>/<row2>')
+def lcd(row1, row2):
+    LcdScreen = lcd()
+    LcdScreen.setUpperText(row1)
+    LcdScreen.setLowerText(row2)
+    return jsonify(True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081, debug=True)
