74353d14c859eaa35bf27c1a20dbd900d7ddc078
xz32 <xz32@users.noreply.github.com>
2016-11-06 06:00:22 +0000

    Add display

---
+++ jp_api/display.py
@@ -0,0 +1,22 @@
+from sensors.lcd import Lcd as Lcd
+from code.otp import OTP as otp
+
+LcdScreen = Lcd()
+codeGenerator = otp('patch of grass')
+
+def setup():
+    LcdScreen.setUpperText('The code is')
+
+def run():
+    while True:
+        LcdScreen.setLowerText(otp.getCode())
+
+def close():
+    LcdScreen.clearDisplay()
+
+if __name__ == "__main__":
+    try:
+        setup()
+        run()
+    except KeyboardInterrupt:
+        close()
