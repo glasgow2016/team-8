8a80357574abd3cdf25d09326bc1333d32445892
xz32 <xz32@users.noreply.github.com>
2016-11-06 00:48:44 +0000

    Refactor

--- jp_api/remote.py
+++ jp_api/remote.py
@@ -7,29 +7,29 @@ from sensors.rain_detection import RainDetection as rain_detection
 from sensors.photoresistor import PhotoResistor as photoresistor
 from sensors.rgb_led import RgbLed as rgb_led
 
+humiditySensor = humiture()
+rainSensor = rain_detection()
+lightSensor = photoresistor()
+rgbLed = rgb_led()
+
 @app.route('/sensor/humidity')
 def humidity():
-    humiditySensor = humiture()
     return jsonify(humiditySensor.getHumidityInPercent())
 
 @app.route('/sensor/temperature')
 def temperature():
-    humiditySensor = humiture()
     return jsonify(humiditySensor.getTemperatureInCeluis())
 
 @app.route('/sensor/rain')
 def rain():
-    rainSensor = rain_detection()
     return jsonify(rainSensor.isRaining())
 
 @app.route('/sensor/light')
 def light():
-    lightSensor = photoresistor()
     return jsonify(lightSensor.getLightValue())
 
 @app.route('/sensor/led/<r>/<g>/<b>')
 def led(r, g, b):
-    rgbLed = rgb_led()
     rgbLed.setColor(r, g, b)
     return jsonify(True)
 
