ea4814ba2f6b8571f0ed60899bc2cc8a68009ef1
xz32 <xz32@users.noreply.github.com>
2016-11-06 07:49:47 +0000

    Revert

--- jp_api/display.py
+++ jp_api/display.py
@@ -3,31 +3,33 @@ from otpcode.otp import OTP as otp
 from sensors.laser import Laser as Laser
 from sensors.ultrasonic_ranging import UltrasonicRanging as Ultrasound
 import time
+import RPi.GPIO as GPIO
 
+GPIO.setWarnings(False)
 LcdScreen = Lcd()
 codeGenerator = otp('patch of grass')
+laserPointer = Laser()
+ultrasoundSensor = Ultrasound()
 
 def run():
-
     timeout = -1
     while True:
         if timeout > 0 and time.time() - timeout < 30:
             LcdScreen.setUpperText('Hi! Your code is')
             LcdScreen.setLowerText('     ' + codeGenerator.getCode() + '     ')
-            Laser().turnOn()
+            laserPointer.turnOn()
             time.sleep(2)
         else:
-            if Ultrasound().getDistanceInCm() < 140:
+            if ultrasoundSensor.getDistanceInCm() < 50:
                 timeout = time.time()
             else:
                 timeout = -1
                 LcdScreen.clearDisplay()
-                Laser().turnOff()
+                laserPointer.turnOff()
 
 def close():
     LcdScreen.clearDisplay()
-    Laser().destroy()
-    Ultrasound().destroy()
+    laserPointer.turnOff()
 
 if __name__ == "__main__":
     try:
