27310b76d43128ac91d54a512b03435614f453a1
xz32 <xz32@users.noreply.github.com>
2016-11-06 06:19:36 +0000

    Base32

--- jp_api/code/otp.py
+++ jp_api/code/otp.py
@@ -1,9 +1,10 @@
 import pyotp
+import base64
 
 class OTP:
 
     def __init__(self, secret):
-        self.totp = pyotp.TOTP(secret.zfill(16))
+        self.totp = pyotp.TOTP(base64.b32encode(secret))
 
     def getCode(self):
         return self.totp.now()
