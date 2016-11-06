0e04a9e66b3a3e3ddc9e10b9e61d5f43bbd3a33f
sheri guo <sheriguo@Sheris-MBP.lan>
2016-11-06 06:21:16 +0000

    #fix_bug fixed data_accessor wrong directory error

--- jp_api/data_accessor.py
+++ jp_api/data_accessor.py
@@ -1,16 +1,16 @@
 #!flask/bin/python
 import json
-with open('data/all_pairs.json','r') as f:
+with open('src_data/all_pairs.json','r') as f:
     SPOTS_WALKING = json.loads(f.read())
 
-with open('data/all_pairs_bicycle.json','r') as f:
+with open('src_data/all_pairs_bicycle.json','r') as f:
     SPOTS_BICYCLE = json.loads(f.read())
 
-with open('data/all_dist_duration_walking.json','r') as f:
+with open('src_data/all_dist_duration_walking.json','r') as f:
     COMBO_WALKING = json.loads(f.read())
 
-with open('data/all_dist_duration_bicycle.json','r') as f:
+with open('src_data/all_dist_duration_bicycle.json','r') as f:
     COMBO_BICYCLE = json.loads(f.read())
 
-with open('data/helix_site.json','r') as f:
+with open('src_data/helix_site.json','r') as f:
     SITE = json.loads(f.read()).get('sights')
