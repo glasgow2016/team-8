84057e11ca54aa39d1a7177c5ce9c9d2cd118ec3
xz32 <xz32@users.noreply.github.com>
2016-11-06 07:25:02 +0000

    Remove local, enhance display

--- jp_api/display.py
+++ jp_api/display.py
@@ -1,18 +1,34 @@
 from sensors.lcd import Lcd as Lcd
 from otpcode.otp import OTP as otp
+from sensors.laser import Laser as Laser
+from sensors.ultrasonic_ranging import UltrasonicRanging as Ultrasound
+import time
 
 LcdScreen = Lcd()
 codeGenerator = otp('patch of grass')
+laserPointer = Laser()
+ultrasoundSensor = Ultrasound()
 
-def setup():
-    LcdScreen.setUpperText('Hi! Your code is')
+timeout = -1
 
 def run():
     while True:
-        LcdScreen.setLowerText('     ' + codeGenerator.getCode() + '     ')
+        if timeout > 0 and time.time() - timeout < 30:
+            LcdScreen.setUpperText('Hi! Your code is')
+            LcdScreen.setLowerText('     ' + codeGenerator.getCode() + '     ')
+            laserPointer.turnOn()
+            time.sleep(2)
+        else:
+            if ultrasoundSensor.getDistanceInCm() < 140:
+                timeout = time.time()
+            else:
+                timeout = -1
+                LcdScreen.clearDisplay()
+                laserPointer.turnOff()
 
 def close():
     LcdScreen.clearDisplay()
+    laserPointer.turnOff()
 
 if __name__ == "__main__":
     try:
